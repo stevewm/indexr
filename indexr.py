f25f798f2f299617f0d138e240950fb8d2781a99
Steve <steve@duh.io>
2018-01-28 22:33:07 +0000

    Refactor for reuse of docker functionality

--- indexr.py
+++ indexr.py
@@ -6,10 +6,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+  return render_template('theme_basic/index.html', containers=get_enabled_containers()) 
+  
+
+@app.route('/monitor')
+def monitor():
+    return render_template('theme_basic/monitor.html', containers=get_enabled_containers()) 
+
+
+def get_enabled_containers():
   client = docker.from_env()
-  urls = [] 
+  enabled_containers = []
   for c in client.containers.list(filters={'label': 'indexr.enable=true'}):
-      l = c.labels
-      icon = url_for('static', filename='icons/' + l['indexr.icon'])
-      urls.append({'name': l['indexr.name'], 'url': l['indexr.url'], 'icon': icon})
-  return render_template('theme_basic/index.html', containers=urls) 
+    l = c.labels
+    icon = url_for('static', filename='icons/' + l['indexr.icon'])
+    enabled_containers.append({'name': l['indexr.name'], 'url': l['indexr.url'], 'icon': icon})
+  return enabled_containers
