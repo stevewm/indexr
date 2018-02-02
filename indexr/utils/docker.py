0a3698221a3f9eccf7483d21a6b9218196363862
Steve <steve@duh.io>
2018-02-02 21:36:52 +0000

    Fix spacing

--- indexr/utils/docker.py
+++ indexr/utils/docker.py
@@ -3,12 +3,12 @@ import docker
 
 class DockerUtils(object):
 
-    def __init__(self):
-        self.client = docker.from_env()
+  def __init__(self):
+    self.client = docker.from_env()
 
-    def get_enabled_containers(self):
-      enabled_containers = []
-      for c in self.client.containers.list(filters={'label': 'indexr.enable=true'}):
-        l = c.labels
-        enabled_containers.append({'name': l['indexr.name'], 'url': l['indexr.url'], 'icon': l['indexr.icon']})
-      return enabled_containers
+  def get_enabled_containers(self):
+    enabled_containers = []
+    for c in self.client.containers.list(filters={'label': 'indexr.enable=true'}):
+      l = c.labels
+      enabled_containers.append({'name': l['indexr.name'], 'url': l['indexr.url'], 'icon': l['indexr.icon']})
+    return enabled_containers
