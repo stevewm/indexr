15dd86e898142bc07efbce87176a62a3423932ad
Steve <steve@duh.io>
2018-05-13 22:19:01 +0100

    Cleanup

--- app/main/routes.py
+++ app/main/routes.py
@@ -3,8 +3,6 @@ from flask import Blueprint, render_template, Response
 from app.utils.docker import DockerUtils
 from app.main import bp
 
-main = Blueprint('main', __name__)
-
 
 @bp.route('/')
 def index():
