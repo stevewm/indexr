1e2d36597b088a85ec72d98a90445b1f6842874b
Steve <steve@duh.io>
2018-05-13 18:47:15 +0100

    Add route for getting background image

--- indexr/main/controllers.py
+++ indexr/main/controllers.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+import requests
+from flask import Blueprint, render_template, Response
 from indexr.utils.docker import DockerUtils
 
 main = Blueprint('main', __name__, template_folder='templates')
@@ -9,3 +10,10 @@ def index():
     dockerutils = DockerUtils()
     enabled_containers = dockerutils.get_enabled_containers()
     return render_template('index.html', containers=enabled_containers)
+
+
+@main.route('potd')
+def potd():
+    r = requests.get('https://source.unsplash.com/random')
+    print(r.content)
+    return Response(r.content, mimetype=r.headers.get('content-type'))
